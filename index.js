8f73d3886e5245aa6e16054e2bc7495649294a81
Deepak <deepaklahoty5d@gmail.com>
2021-10-20 14:29:11 +0530

    added

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const http =require('http');
+const port = 3000;
+const hostname ='0.0.0.0';
 const bodyParser = require('body-parser');
 var empctrl= require('./controllers/stockController.js')
 const {mongoose}=require('./db.js');
 var app = express();
 app.use(bodyParser.json());
-app.listen(3000,()=>console.log("server started"));
-app.use('/emp',empctrl)
+app.listen(port,hostname,()=>console.log("server started"));
+app.use('/emp',empctrl);
+[x]
