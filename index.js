c582c8c1566746cbd622443cd091c1bae03aa6a7
Deepak <deepaklahoty5d@gmail.com>
2021-10-22 18:26:05 +0530

    last update

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+const cors = require('cors');
 const express = require('express');
+var app = express();
+app.use(cors());
 const http =require('http');
 const port = process.env.PORT;
 const hostname ='0.0.0.0';
 const bodyParser = require('body-parser');
 var empctrl= require('./controllers/stockController.js')
 const {mongoose}=require('./db.js');
-var app = express();
 app.use(bodyParser.json());
 app.listen(port,hostname,()=>console.log("server started"));
 app.use('/emp',empctrl);
