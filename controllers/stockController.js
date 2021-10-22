3005986ea1f56f12402d3119f1f7c827f9074b00
Deepak <deepaklahoty5d@gmail.com>
2021-10-22 16:13:13 +0530

    put and delete

--- controllers/stockController.js
+++ controllers/stockController.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 var express = require('express');
 var router=express.Router();
-
+var ObjectId = require('mongoose').Types.ObjectId;
 var {stock}=require('../models/employee.js')
 console.log("i m here");
 router.get('/',(req,res)=>{
@@ -19,7 +19,7 @@ router.get('/',(req,res)=>{
 }
 );
 
-router.post('/e',(req,res)=>{
+router.post('/',(req,res)=>{
     var stockpost=new stock({
         company:req.body.company,
         number :req.body.number,
@@ -33,4 +33,33 @@ router.post('/e',(req,res)=>{
 
 
 });
+
+router.put('/:id', (req, res) => {
+    if (!ObjectId.isValid(req.params.id))
+        return res.status(400).send(`No record with given id : ${req.params.id}`);
+
+    var updatedata = {
+        company:req.body.company,
+        number :req.body.number,
+        type :req.body.type,
+        quantity :req.body.quantity,
+    };
+    stock.findByIdAndUpdate(req.params.id, { $set: updatedata }, { new: true }, (err, doc) => {
+        if (!err) { res.send(doc); }
+        else { console.log('Error in Stock Update :' + JSON.stringify(err, undefined, 2)); }
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    if (!ObjectId.isValid(req.params.id))
+        return res.status(400).send(`No record with given id : ${req.params.id}`);
+
+    stock.findByIdAndRemove(req.params.id, (err, doc) => {
+        console.log("valid id")
+        if (!err) { res.send(doc); }
+        else { console.log('Error in Employee Delete :' + JSON.stringify(err, undefined, 2)); }
+    });
+});
+
+
 module.exports=router;
